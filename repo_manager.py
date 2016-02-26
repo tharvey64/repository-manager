020fcba09cc1c500c79a6851ce096aa73f1ccf44
Thomas Harvey <thomasharvey24@gmail.com>
2016-02-26 16:32:59 -0500

    edit to gitignore

--- repo_manager.py
+++ repo_manager.py
@@ -4,16 +4,20 @@ try:
     from local_config import config
 except ImportError:
     sys.stderr.write(
-"""
+'''
 Warning: Can't find the file 'local_config.py' in the directory
 containing {}. It appears you've customized things. Create {} in
 the directory containing repo_manager.py and file the format provided
 in local_config.py.md.
-""".format(__file__))
-    sys.stderr.write("\nFor debugging purposes, the exception was:\n\n")
+'''.format(__file__))
+    sys.stderr.write('\nFor debugging purposes, the exception was:\n\n')
     traceback.print_exc()
 
 
 _ORG = 'ByteAcademyCo'
 
-def 
+
+github = GithubAPI(config.get('access_token'))
+
+def test_list_repos():
+    path = 
