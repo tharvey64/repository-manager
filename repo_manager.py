fdf5ea1fcf8377616e90bd7da32db3873f7b73af
Thomas Harvey <thomasharvey24@gmail.com>
2016-02-26 18:33:54 -0500

    collecting repos

--- repo_manager.py
+++ repo_manager.py
@@ -1,5 +1,5 @@
+import re
 from github_api import GithubAPI
-
 try:
     from local_config import config
 except ImportError:
@@ -19,5 +19,31 @@ _ORG = 'ByteAcademyCo'
 
 github = GithubAPI(config.get('access_token'))
 
-def test_list_repos():
-    path = 
+def test_organizations_list_repos(github, page=1):
+    path = 'orgs/{org}/repos'.format(org=_ORG)
+    return github.get(path,params={'type':'all','per_page':'100','page':page})
+
+def collect_all_repos(github):
+    # path = 'orgs/{org}/repos'.format(org=_ORG)
+    response = test_organizations_list_repos(github)
+    repos = response.json()
+    links = get_links(response.headers.get('Link'))
+    while links.get('last',False):
+        # print(response.content)
+        print(response.headers)
+        response = test_organizations_list_repos(github, page=links['next'])
+        links = get_links(response.headers.get('Link'))
+        repos+=response.json()
+        print(len(repos))
+def get_links(link_header_str):
+    link_header = (parse_link(link_str) for link_str in link_header_str.split(','))
+    # print(len(link_header))
+    link_info = {link_info['rel']: link_info['page'] for link_info in link_header}
+    return link_info
+
+def parse_link(link):
+    url, relation = link.split(';')
+    return  {'page': get_page(url),'rel':relation.split('=')[1].strip('"')}
+
+def get_page(url):
+    return re.search(r'(?<=page=)(?<!per_page=)\d*',url).group()
