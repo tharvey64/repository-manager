be48c8c502f71c4175c9a46c86a86770782793c7
Thomas Harvey <thomasharvey24@gmail.com>
2016-02-26 21:29:38 -0500

    billy help

--- repo_manager.py
+++ repo_manager.py
@@ -16,53 +16,60 @@ in local_config.py.md.
 
 _ORG = 'ByteAcademyCo'
 
-
 github = GithubAPI(config.get('access_token'))
 
-def filter_repositories(repositories, *args):
-    '''
-    args will be filters
-    they must be callable
-    '''
-    cleaned_repos = []
-    for repo in repositories:
-        if all(filter_(repo) for filter_ in args):
-            cleaned_repos.append(repo)
-    return cleaned_repos
+class Repository:
+    def __init__(self,**kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-def name_startswith_filter(string):
-    def name_filter(item):
-        return item.get('name','').startswith(string)
-    return name_filter
+class FilterSet:
+    def __init__(self, **kwargs):
+        self.filters = [{'name':key,'filter':value} for key,value in kwargs.items()]
 
-def collect_all_repos(github):
-    response = get_repos(github)
-    repos = response.json()
-    links = get_links(response.headers.get('Link'))
-    while links.get('last',False):
-        response = get_repos(github, page=links['next'])
-        links = get_links(response.headers.get('Link'))
-        repos+=response.json()
-        print(len(repos))
-    return repos
+    def clean(self, item):
+        return all(rule['filter'](item) for rule in self.filters)
 
-def get_repos(github, page=1):
-    path = 'orgs/{org}/repos'.format(org=_ORG)
-    return github.get(path,params={'type':'all','per_page':'100','page':page})
+    def add_filter(self, name, func):
+        self.filters.append({'name':name,'filter':func})
 
-def get_links(link_header_str):
-    link_header = parse_link_header(link_header_str)
-    link_info = {link_info['rel']: link_info['page'] for link_info in link_header}
-    return link_info
 
-def parse_link_header(link_header):
-    links = (link_str.split(';') for link_str in link_header.split(','))
-    return  ({
-        'page': get_page(url),
-        'rel':relation.split('=')[1].strip('"')}
-        for url,relation in links
-    )
+class Collector:
+    def __init__(self, github):
+        self.github = github
+        self.org = _ORG
+        self.repositories = []
 
-def get_page(url):
-    return re.search(r'(?<=page=)(?<!per_page=)\d*',url).group()
+    def collect_all_repos(self):
+        response = self.get_repos()
+        repos = response.json()
+        links = self.get_links(response.headers.get('Link'))
 
+        while links.get('last', False):
+            response = self.get_repos(page=links['next'])
+            links = self.get_links(response.headers.get('Link'))
+            repos += response.json()
+            print(len(repos))
+        return repos
+    
+    def get_repos(self, page=1):
+        path = 'orgs/{org}/repos'.format(org=_ORG)
+        return self.github.get(path,params={'type':'all','per_page':'100','page':page})
+    
+    def get_links(self, link_header_str):
+        link_header = self.parse_link_header(link_header_str)
+        link_info = {link_info['rel']: link_info['page'] for link_info in link_header}
+        return link_info
+
+    def parse_link_header(self, link_header):
+        links = (link_str.split(';') for link_str in link_header.split(','))
+        return  (
+            {
+                'page': re.search(r'(?<=page=)(?<!per_page=)\d*',url).group(),
+                'rel': relation.split('=')[1].strip('"')
+            }
+            for url,relation in links
+        )
+
+    def run(self):
+        self.repositories = self.collect_all_repos()
---
+++ sand_box.py
@@ -0,0 +1,29 @@
+import repo_manager
+import repo_filters
+
+import subprocess
+
+
+def clone_and_create_branch(repo):
+    command = 'cd ../repositories && git clone {clone_url}'.format(clone_url=repo.clone_url)
+    subprocess.call(command.split())
+    command = 'git -C {path} checkout -b test-update'.format(path='../repositories/'+repo.name)
+    subprocess.call(command.split())
+
+def main():
+    collector = repo_manager.Collector(repo_manager.github)
+    collector.run()
+    matchers = dict(name_startswith=repo_filters.name_startswith_filter('exercise'))
+    filter_set = repo_manager.FilterSet(**matchers)
+    
+    repositories = []
+    for repo in collector.repositories:
+        if filter_set.clean(repo):
+            repositories.append(repo_manager.Repository(**repo))
+    return repositories
+
+
+if __name__ == "__main__":
+    repos = main()
+    for r in repos:
+        clone_and_create_branch(r)
