72e54a154a47f87000bd6d9fb8e27841bb3697d7
Thomas Harvey <thomasharvey24@gmail.com>
2016-02-29 03:32:53 -0500

    added some more stuff to sandbox

--- sand_box.py
+++ sand_box.py
@@ -4,11 +4,29 @@ import repo_filters
 import subprocess
 
 
-def clone_and_create_branch(repo):
+def git_clone_and_create_branch(repo):
     command = 'cd ../repositories && git clone {clone_url}'.format(clone_url=repo.clone_url)
     subprocess.getoutput(command)
     command = 'git -C {path} checkout -b test-update'.format(path='../repositories/'+repo.name)
     subprocess.getoutput(command)
+
+def git_add(repo):
+    command = 'git -C {path} add .'.format(path='../repositories/'+repo.name)
+    subprocess.getoutput(command)
+
+def git_commit(repo):
+    command = 'git -C {path} commit -m "adding edits"'.format(path='../repositories/'+repo.name)
+    subprocess.getoutput(command)
+
+def git_push(repo):
+    command = 'git -C {path} push origin test-update'.format(path='../repositories/'+repo.name)
+    subprocess.getoutput(command)
+
+# make pull repuest that pulls test-update into master
+# close pull request
+# get the SHA from closing that pull request
+# create new release at this SHA
+# FORK TO BYTE EXERCISES
 
 def main():
     collector = repo_manager.Collector(repo_manager.github)
