ba23374ab7f1ef2252924f405fedd5b2be5e6a77
Thomas Harvey <thomasharvey24@gmail.com>
2016-03-06 21:37:56 -0500

    adding git commands

--- repo_manager.py
+++ repo_manager.py
@@ -1,4 +1,5 @@
 import re
+import json
 from github_api import GithubAPI
 try:
     from local_config import config
@@ -7,7 +8,7 @@ except ImportError:
 '''
 Warning: Can't find the file 'local_config.py' in the directory
 containing {file}. It appears you've customized things. Create {file} in
-the directory containing repo_manager.py and file the format provided
+the directory containing repo_manager.py following the format provided
 in local_config.py.md.
 '''.format(file=__file__))
     sys.stderr.write('\nFor debugging purposes, the exception was:\n\n')
@@ -18,10 +19,107 @@ _ORG = config.get('organization')
 
 github = GithubAPI(config.get('access_token'))
 
+class GitLocal:
+    @staticmethod
+    def git_clone_and_create_branch(repo):
+        command = 'cd ../repositories && git clone {clone_url}'.format(clone_url=repo.clone_url)
+        subprocess.getoutput(command)
+        command = 'git -C {path} checkout -b test-update'.format(path='../repositories/'+repo.name)
+        subprocess.getoutput(command)
+
+    @staticmethod
+    def git_add(repo):
+        command = 'git -C {path} add .'.format(path='../repositories/'+repo.name)
+        subprocess.getoutput(command)
+
+    @staticmethod
+    def git_commit(repo):
+        command = 'git -C {path} commit -m "adding edits"'.format(path='../repositories/'+repo.name)
+        subprocess.getoutput(command)
+
+    @staticmethod
+    def git_push(repo):
+        command = 'git -C {path} push origin test-update'.format(path='../repositories/'+repo.name)
+        subprocess.getoutput(command)
+
+
+class GithubAPIExtension(GithubAPI):
+    def __init__(self, access_token):
+        super().__init__(access_token)
+        self.__log = []
+
+    def get_log(self):
+        self.__log, temp = [], self.__log
+        return temp
+
+    def git_api_pull_request(self, repo, org):
+        path = 'repos/{owner}/{repo}/pulls'.format(owner=_ORG, repo=repo.name)
+        data = json.loads({
+            'title':'Adding Tests.',
+            'head':'{organization}:test-update'.format(organization=_ORG),
+            'base':'{organization}:master'.format(organization=_ORG),
+            'body':'First iteration of test error messages.'
+        })
+        response = self.post(path, data=data)
+
+        self.__log.append(response)
+
+        if str(response.status_code)[0] == "2": 
+            data = response.json()
+        else:
+            data = {}
+        return data.get('number')
+
+    def git_api_confirm_pull_request(self, repo):
+        """ 
+        Get if a pull request has been merged
+        GET repos/:owner/:repo/pulls/:number/merge
+        """
+        path = 'repos/{owner}/{repo}/pulls/{number}/merge'.format(owner=_ORG,repo=repo.name,number=repo.info.get('number'))
+        response = self.get(path)
+        if str(response.status_code)[0] == "2": 
+            data = response.json()
+        else:
+            data = {}
+        return data
+
+    def git_api_merge_pull_request(self, repo):
+        """
+        merge pull request
+        PUT /repos/:owner/:repo/pulls/:number/merge
+        @param commit_message  string  Extra detail to append to automatic commit message.
+        @param sha string  SHA that pull request head must match to allow merge 
+        Response if successful 
+        Status 200
+        {
+          "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+          "merged": true,
+          "message": "Pull Request successfully merged"
+        }
+
+
+        Response if merge cannot be performed
+        Status 405 Method Not Allowed
+        {
+          "message": "Pull Request is not mergeable",
+          "documentation_url": "https://developer.github.com/v3/pulls/#merge-a-pull-request-merge-button"
+        }
+
+        Response if sha was provided and pull request head did not match
+        Status 409 Conflict
+        {
+          "message": "Head branch was modified. Review and try the merge again.",
+          "documentation_url": "https://developer.github.com/v3/pulls/#merge-a-pull-request-merge-button"
+        }
+        """
+
+githubEXT = GithubAPIExtension(config.get('access_token'))
+
 class Repository:
     def __init__(self,**kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        self.info = {}
 
 class FilterSet:
     def __init__(self, **kwargs):
--- sand_box.py
+++ sand_box.py
@@ -4,24 +4,6 @@ import repo_filters
 import subprocess
 
 
-def git_clone_and_create_branch(repo):
-    command = 'cd ../repositories && git clone {clone_url}'.format(clone_url=repo.clone_url)
-    subprocess.getoutput(command)
-    command = 'git -C {path} checkout -b test-update'.format(path='../repositories/'+repo.name)
-    subprocess.getoutput(command)
-
-def git_add(repo):
-    command = 'git -C {path} add .'.format(path='../repositories/'+repo.name)
-    subprocess.getoutput(command)
-
-def git_commit(repo):
-    command = 'git -C {path} commit -m "adding edits"'.format(path='../repositories/'+repo.name)
-    subprocess.getoutput(command)
-
-def git_push(repo):
-    command = 'git -C {path} push origin test-update'.format(path='../repositories/'+repo.name)
-    subprocess.getoutput(command)
-
 # make pull repuest that pulls test-update into master
 # close pull request
 # get the SHA from closing that pull request
@@ -43,5 +25,6 @@ def main():
 
 if __name__ == "__main__":
     repos = main()
-    for r in repos:
-        clone_and_create_branch(r)
+    git = repo_manager.githubEXT
+    # for r in repos:
+        # githubEXT
